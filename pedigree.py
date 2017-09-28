e9ed1c42ae9855f77cc5d35865a397c556c6b04b
Gabriela Brown <gabcbrown@gmail.com>
2017-09-28 16:03:03 -0400

    Simulate meiosis

--- pedigree.py
+++ pedigree.py
@@ -1,3 +1,5 @@
+# TODO:
+# More crossover points
 from random import choice
 
 class Individual():
@@ -11,8 +13,26 @@ class Individual():
         self.genome = genome
 
     def inherit(self):
-        m = choice([0,1])
-        p = choice([0,1])
-        matriline = self.mother.genome[m]
-        patriline = self.father.genome[p]
-        self.genome = [matriline, patriline]
+        maternal = self._meiosis(self.mother.genome[0], self.mother.genome[1])
+        paternal = self._meiosis(self.father.genome[0], self.father.genome[1])
+        self.genome = [maternal, paternal]
+
+    # Takes two homologous chromosomes (hc) and simulates meiosis. Returns a single
+    # gamete chromosome. Simulates 1 crossover point.
+    def _meiosis(self, hc1, hc2):
+        # Randomly chooses an index to crossover at.
+        crossoverPoint = choice(range(0,len(hc1)))
+
+        # Simulate Meiosis I with:
+        # - each homologous chromosome duplicates itself into two identical
+        #   sister chromatids (sc).
+        # - One sister chromatid from each homologous chromosome cross over.
+        sc1a, sc1b, sc2a, sc2b = hc1, hc1, hc2, hc2 # TODO: make sure this doesn't break
+        new_sc1a = sc1a[:crossoverPoint] + sc2a[crossoverPoint:]
+        new_sc2a = sc2a[:crossoverPoint] + sc1a[crossoverPoint:]
+
+        # Simulate Meiosis II with
+        # - the now crossed-over homologous chromosomes separate and the cell
+        #   divides.
+        # - the sister chromatids separate and the cells divide into 4 gametes.
+        return choice([new_sc1a, sc1b, new_sc2a, sc2b])
